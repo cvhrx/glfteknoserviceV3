d930bb45054e01b61682243c56368c7703ae5ef1
cvhrx <chrismendoza764@gmail.com>
2025-10-23 23:34:20 +0200

    Update service worker cache name and event listeners

--- service-worker_v4.js
+++ service-worker_v4.js
@@ -1,5 +1,3 @@
-const CACHE="glf-final-patch-v4";
-const ASSETS=["/","/index.html","/styles.css","/app.js","/firebase.js","/manifest.json","/assets/logo.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k))))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="rapportini-v4";
+self.addEventListener("install",e=>e.waitUntil(self.skipWaiting()));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
